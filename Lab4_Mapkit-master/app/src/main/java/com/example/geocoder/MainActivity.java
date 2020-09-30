6de4cb0c3d42ad07325fd199263b2a4af882035e
Timur <you@example.com>
2020-09-30 12:36:58 +1000

    Initial commit

---
+++ Lab4_Mapkit-master/app/src/main/java/com/example/geocoder/MainActivity.java
@@ -0,0 +1,133 @@
+package com.example.geocoder;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraListener;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.map.CameraUpdateSource;
+import com.yandex.mapkit.map.Map;
+import com.yandex.mapkit.map.MapObjectCollection;
+import com.yandex.mapkit.map.VisibleRegionUtils;
+import com.yandex.mapkit.mapview.MapView;
+import com.yandex.mapkit.search.Response;
+import com.yandex.mapkit.search.SearchFactory;
+import com.yandex.mapkit.search.SearchManagerType;
+import com.yandex.mapkit.search.SearchOptions;
+import com.yandex.mapkit.search.SearchManager;
+import com.yandex.mapkit.search.Session;
+import com.yandex.runtime.Error;
+import com.yandex.runtime.image.ImageProvider;
+
+public class MainActivity extends Activity implements Session.SearchListener, CameraListener {
+
+    private final String API_KEY = "ed6e88ed-ef0b-4024-9733-aba7bfff396b";
+
+    private MapView mapView;
+    private EditText searchEdit;
+    private SearchManager searchManager;
+    private Session searchSession;
+
+
+
+
+    private void submitQuery(String query) {
+        searchSession = searchManager.submit(
+                query,
+                VisibleRegionUtils.toPolygon(mapView.getMap().getVisibleRegion()),
+                new SearchOptions(),
+                this);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        MapKitFactory.setApiKey(API_KEY);
+        MapKitFactory.initialize(this);
+        SearchFactory.initialize(this);
+
+        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+
+        searchManager = SearchFactory.getInstance().createSearchManager(SearchManagerType.COMBINED);
+
+        mapView = (MapView)findViewById(R.id.mapview);
+        mapView.getMap().addCameraListener(this);
+
+        searchEdit = (EditText)findViewById(R.id.textSearch);
+        searchEdit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    submitQuery(searchEdit.getText().toString());
+                }
+
+                return false;
+            }
+        });
+
+        mapView.getMap().move(
+                new CameraPosition(new Point(43.1162355,131.9039189), 12.0f, 0.0f, 0.0f));
+                new Animation(Animation.Type.SMOOTH, 5);
+
+
+        submitQuery(searchEdit.getText().toString());
+    }
+
+    @Override
+    protected void onStop() {
+        mapView.onStop();
+        MapKitFactory.getInstance().onStop();
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        MapKitFactory.getInstance().onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onSearchResponse(@org.jetbrains.annotations.NotNull Response response) {
+        MapObjectCollection mapObjects = mapView.getMap().getMapObjects();
+        mapObjects.clear();
+
+   Point resultLocation = response.getCollection().getChildren().get(0).getObj().getGeometry().get(0).getPoint();
+
+            if (resultLocation != null) {
+                mapObjects.addPlacemark(
+                        resultLocation,
+                        ImageProvider.fromResource(this, R.drawable.map));
+
+
+                mapView.getMap().move(
+                        new CameraPosition(new Point(resultLocation.getLatitude(), resultLocation.getLongitude()), 14.0f, 0.0f, 0.0f));
+                new Animation(Animation.Type.SMOOTH, 5);
+
+            }
+        }
+
+    @Override
+    public void onSearchError(@NonNull Error error) {
+
+    }
+
+
+    @Override
+    public void onCameraPositionChanged(
+            Map map,
+            CameraPosition cameraPosition,
+            CameraUpdateSource cameraUpdateSource,
+            boolean finished) {
+        if (finished) {
+            submitQuery(searchEdit.getText().toString());
+        }
+    }
+}
